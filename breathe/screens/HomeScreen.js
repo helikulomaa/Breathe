92fa755d301efa8a56ef391577fda8f52522190f
Heli Kulomaa <heli.kulomaa@gmail.com>
2025-04-08 21:55:14 +0300

    Add buttons to HomeScreen

--- breathe/screens/HomeScreen.js
+++ breathe/screens/HomeScreen.js
@@ -5,7 +5,10 @@ export default function HomeScreen({ navigation }) {
     return (
         <View style={styles.container}>
             <Text>Breathe</Text>
-            <Button title="1 min" onPress={() => navigation.navigate('BreatheScreen')} />
+            <Button title="1 min" onPress={() => navigation.navigate('BreatheScreen', { duration: 1 })} />
+            <Button title="3 min" onPress={() => navigation.navigate('BreatheScreen', { duration: 3 })} />
+            <Button title="5 min" onPress={() => navigation.navigate('BreatheScreen', { duration: 5 })} />
+            <Button title="10 min" onPress={() => navigation.navigate('BreatheScreen', { duration: 10 })} />
         </View>
     );
 }
